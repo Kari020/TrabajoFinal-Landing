2bd390b7d4427c8c1b0e82252b0e0e0efae5c6fc
Kari <yeilet05@gmail.com>
2023-04-18 13:22:26 -0300

    Primer Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+const formularioUsuarioHTML = document.getElementById("alerta-enviado");
+
+formularioUsuarioHTML.addEventListener("submit", (event) =>{
+    event.preventDefault();
+  alert("Tu mensaje se envió con éxito");   })
+
